72ca5903400df1abd667f423235aea0b53d21b81
mostafa <mostafamnbvcxz5@gmail.com>
2021-12-07 17:24:55 +0330

    make menu working

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('.menu');
+const menu = document.querySelector('.left-bar')
+button.addEventListener("click",function(){
+    menu.classList.toggle('show');
+});
